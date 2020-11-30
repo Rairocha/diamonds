515a4caed99647da8f54cdfcc3bf40d9ee3e6ee1
Your Name <you@example.com>
2020-11-30 09:00:59 -0300

    first commit

---
+++ diamonds.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+from sklearn.metrics import mean_squared_error
+
+in_file=st.file_uploader('Coloque aqui o seu arquivo csv')
+correct_diamonds= pd.read_csv('data/rick_diamonds_gabarito.csv')
+try:
+    test_diamonds=pd.read_csv(in_file)
+except:
+    st.write('Por favor coloque um arquivo csv abestado')
+else:
+    t=test_diamonds[0:4000]
+    c=correct_diamonds[0:4000]
+    m=mean_squared_error(c['price'], t['price_predicted'])
+    st.write(f'Your mean squared error is {m}')
