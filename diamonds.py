96b55fe4b1cfc1c90eab691162f17e6418f0920f
Your Name <you@example.com>
2020-11-30 11:49:54 -0300

    celebration in diamonds.py

--- diamonds.py
+++ diamonds.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.metrics import mean_squared_error
 import numpy as np
 
+st.image('data/rick_crew.jpeg',use_column_width =True)
 in_file=st.file_uploader('Coloque aqui o seu arquivo csv')
 correct_diamonds= pd.read_csv('data/rick_diamonds_gabarito.csv')
 try:
@@ -14,3 +15,5 @@ else:
     c=correct_diamonds[0:4000]
     m=np.sqrt(mean_squared_error(c['price'], t['price_predicted']))
     st.write(f'Your mean squared error is {m}')
+    if m <900:
+        st.balloons()
